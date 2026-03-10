80a46e3a5c3a0362303097275f268ecc3c6c4f0e
Sonni17 <sonyaeasanchez@outlook.com>
2026-03-10 05:39:57 -0400

    js references

--- public/script.js
+++ public/script.js
@@ -47,3 +47,20 @@ function alertFun(){
 
   let img = document.querySelector('img');
   img.addEventListener('load', loaded);
+
+  let obj2 = myobj;
+
+//obj2 and myobj are the same variable
+
+obj2.name = "Shelly";
+
+console.log(myobj.name);//Shelly
+
+//if you need to create a new object and copy its values use Object.assign
+
+let obj3 = {};
+Object.assign(obj3, myobj);
+
+obj3.name = 'Smith';
+
+console.log(myobj.name, obj3.name);//Shelly, Smith
