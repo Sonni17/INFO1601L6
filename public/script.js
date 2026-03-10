4f65c374d367c1103c325451c475309037c6cfd5
Sonni17 <sonyaeasanchez@outlook.com>
2026-03-10 05:35:32 -0400

    mouse over func

--- public/script.js
+++ public/script.js
@@ -16,29 +16,27 @@ function alertFun(){
     alert(`You've entered: ${val}`);
    }
 
-   //get client data using navigator
-console.log(window.navigator.cookieEnabled);
-console.log(window.navigator.onLine);
-console.log(navigator.appVersion);
-console.log(navigator.userAgent)
-console.log(navigator.platform);
+  function myFun(){
+    alert("hello");  
+  }
 
-//get window metadata using window
-console.log(window.location.href);//get full url
-console.log(window.location.protocol);
-console.log(window.location.hostname);
+  function myFun2(){
+    console.log("myFun2 called");
+  }
 
-function redirect(url){
-  window.location.assign(url);//redirects the page to another url
-}
+ //receives the event parameter from addEventListener high order function
+  function logEventType(event){
+    console.log(event.type);
+  }
 
-window.onload = function(event){
-   console.log("Page has loaded");
-   //do other javascript stuff here
-}
+  let myBtn = document.querySelector("#myBtn");
 
-const result = document.querySelector('#result');
-result.innerHTML = '<h2>My Span</h2>';
-result.style.color = 'blue';
+  //attach myFun2 in addition to myFun to the click event of myBtn
+  myBtn.addEventListener("click", myFun2);
 
-<input id="myBtn" type="button" onclick="myFun()" value="click me"/>
+  //There are other events such as mouseover and mouseout
+  
+  //Any callback passed to addeventListener receives an event object
+  myBtn.addEventListener("mouseover", logEventType);//logs 'mouseover'
+  myBtn.addEventListener("mouseout", logEventType);// logs 'mouseout'
+ //these events will fire when the mouse pointer hovers/passes over the button
