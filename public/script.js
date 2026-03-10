09a756e0d0e670d50ced875ee7e27250b600588a
Sonni17 <sonyaeasanchez@outlook.com>
2026-03-10 05:44:40 -0400

    js and html changes

--- public/script.js
+++ public/script.js
@@ -64,3 +64,22 @@ Object.assign(obj3, myobj);
 obj3.name = 'Smith';
 
 console.log(myobj.name, obj3.name);//Shelly, Smith
+
+// if a record looks like.
+let record = {
+   id: 1,
+   name: "bob's",
+   brewery_type: "fire",
+   website_url:"https://bobs.com"
+} 
+
+//*note the use of backticks
+//using string interpolation
+//a row in the table would look like;
+
+let row = `<tr> 
+    <td>${record.id}</td>
+    <td>${record.name}</td>
+    <td>${record.brewery_type}</td>
+    <td>${record.website_url}</td>    
+</tr>`;
