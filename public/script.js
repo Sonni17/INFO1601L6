35757834548cb9281e2fdbd59b59cdb39c23beaa
Sonni17 <sonyaeasanchez@outlook.com>
2026-03-10 05:28:06 -0400

    js error

---
+++ public/script.js
@@ -0,0 +1,17 @@
+function alertFun(){
+    alert("Hello !");
+   }
+
+   function confirmFun(){
+     let didConfirm = confirm("Are you sure about that?");
+     if(didConfirm){
+       alert("You confirmed :)");
+     }else{
+      alert("You did not confirm :(");
+     }
+   }
+
+   function promptFun(){
+    let val = prompt("What is your name?");
+    alert(`You've entered: ${val}`);
+   }
